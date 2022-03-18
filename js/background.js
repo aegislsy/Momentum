1aaadde93c8f051b36d0ef4f255daea90bed2df8
aegislsy <leesaeyim@naver.com>
2022-03-18 17:07:58 +0900

    background

--- js/background.js
+++ js/background.js
@@ -1,9 +1,8 @@
 const images = ["01.jpg","02.jpg","03.jpg"];
-
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+function change(){
+document.body.style.backgroundImage= "url( img/" + chosenImage + ")";
+}
 
-bgImage.src= `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+change();
